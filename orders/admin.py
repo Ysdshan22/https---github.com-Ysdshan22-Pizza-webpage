41f21cfb8803b6446138d7071c74acc4920a6198
Yamuna <ysdshan22@gmail.com>
2026-03-25 08:41:41 +0100

    Added models: Pizza, Topping, Drink, Order, OrderItem

--- orders/admin.py
+++ orders/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
-from .models import Pizza
+from .models import Pizza, Topping, Drink, Order, OrderItem
 
 admin.site.register(Pizza)
+admin.site.register(Topping)
+admin.site.register(Drink)
+admin.site.register(Order)
+admin.site.register(OrderItem)
--- orders/models.py
+++ orders/models.py
@@ -10,3 +10,51 @@ class Pizza(models.Model):
 
     def __str__(self):
         return self.name
+    
+class Topping(models.Model):
+    name = models.CharField(max_length=100)
+    price_small = models.DecimalField(max_digits=5, decimal_places=2)
+    price_medium = models.DecimalField(max_digits=5, decimal_places=2)
+    price_large = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+    
+class Drink(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+    
+from django.contrib.auth.models import User  # make sure this is at top
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+    total_price = models.DecimalField(max_digits=7, decimal_places=2)
+    status = models.CharField(max_length=50)
+    delivery_address = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"Order {self.id}"
+    
+class OrderItem(models.Model):
+    SIZE_CHOICES = [
+        ('S', 'Small'),
+        ('M', 'Medium'),
+        ('L', 'Large'),
+    ]
+
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+
+    pizza = models.ForeignKey(Pizza, on_delete=models.SET_NULL, null=True, blank=True)
+    drink = models.ForeignKey(Drink, on_delete=models.SET_NULL, null=True, blank=True)
+
+    quantity = models.IntegerField()
+    size = models.CharField(max_length=1, choices=SIZE_CHOICES)
+
+    toppings = models.ManyToManyField(Topping, blank=True)
+
+    def __str__(self):
+        return f"Item {self.id}"
